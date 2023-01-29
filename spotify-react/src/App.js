3f7e9f68fa309f68f88b2a9fa71de2472a27487f
caleb cooper <calebcooper86@gmail.com>
2023-01-29 19:18:59 +0000

    adding tailwind to the project scope

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
+      <h1 className='text-3xl font-bold underline'>
+        Hello world
+      </h1>
       </div>
   );
 }
