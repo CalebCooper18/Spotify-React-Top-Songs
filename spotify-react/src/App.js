d650adcfcd94a28b3ef01e4f604c9210823affc8
caleb cooper <calebcooper86@gmail.com>
2023-01-29 19:49:53 +0000

    made link to spotify auth

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -1,11 +1,19 @@
+import { useEffect, useState } from 'react';
 import './index.css';
 
+
 function App() {
+
+  const CLINET_ID = '03aeaded767a49b483a3fb235cc3a196'
+  const REDIRECT_URI = 'http://localhost:3000';
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = 'token'
+
+
   return (
-    <div className="App">
-      <h1 className='text-3xl font-bold underline'>
-        Hello world
-      </h1>
+    <div>
+      <a href={`${AUTH_ENDPOINT}?client_id=${CLINET_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+      >Login Into Spotify</a>
       </div>
   );
 }
